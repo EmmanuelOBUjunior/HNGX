f7ec1e89445c559cad094996c77c842fd376177f
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-09-08 10:22:15 +0000

    Reworks on the day

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ const dayElement = document.getElementById("date");
  * @param {Date} date 
  */
 function timeUpdate(){
-    const hours12 = new Date().getHours() % 12 || 12;
-    const minutes = new Date().getMinutes();
-    const seconds = new Date().getSeconds();
-    const isAm = hours12 < 12;
+    // const hours12 = new Date().getHours() % 12 || 12;
+    // const minutes = new Date().getMinutes();
+    // const seconds = new Date().getSeconds();
+    // const isAm = hours12 < 12;
 
-    return `${hours12.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}: ${seconds.toString().padStart(2, "0")} ${isAm ?"AM":"PM"}`
+    // return `${hours12.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}: ${seconds.toString().padStart(2, "0")} ${isAm ?"AM":"PM"}`
 }
 
 function dateUpdate(){
