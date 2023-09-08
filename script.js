e7439d72696d59884b5ab317f5973b138530561f
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-09-08 10:26:39 +0000

    Reworks on the time function

--- script.js
+++ script.js
@@ -5,12 +5,6 @@ const dayElement = document.getElementById("date");
  * @param {Date} date 
  */
 function timeUpdate() {
-    // const hours12 = new Date().getHours() % 12 || 12;
-    // const minutes = new Date().getMinutes();
-    // const seconds = new Date().getSeconds();
-    // const isAm = hours12 < 12;
-
-    // return `${hours12.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}: ${seconds.toString().padStart(2, "0")} ${isAm ?"AM":"PM"}`
     const hours = new Date().getHours() * 60 * 60 * 1000
     const minutes = new Date().getMinutes() * 60 * 1000
     const seconds = new Date().getSeconds()* 1000
