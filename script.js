49c9504c6da4e155fd10b3bcd909b7163fd8f5d0
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-09-08 08:41:15 +0000

    Working on the time function

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const dayElement = document.getElementById("date");
 /**
  * @param {Date} date 
  */
-function timeUpdate(date){
-    const hours12 = date.getHours();
+function timeUpdate(){
+    const hours12 = new Date().getHours() % 12 || 12;
+    const minutes = new Date().getMinutes();
+    const isAm = hours12 < 12;
 
-    return hours12
+    return `${hours12.toString().padStart(2, '0')}:${minutes} ${isAm}`
 }
 
 console.log(timeUpdate(date));
