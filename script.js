d8c621c50e28ff0622225e65fbed8a2a5a03fb01
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-09-08 07:50:09 +0000

    Declaring the dayelement in script file

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 const timeElement = document.getElementById("time");
+const dayElement = document.getElementById("date");
 console.log(timeElement.textContent);
+console.log(dayElement.textContent);
